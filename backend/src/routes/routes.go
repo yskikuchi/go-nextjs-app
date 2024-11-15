0eafb35891b1eb25faec245d92b0d32509190984
ykikuchi <y.kikuchi5101@gmail.com>
2024-11-15 18:49:19 +0900

    refactor: コメントを追加

--- backend/src/routes/routes.go
+++ backend/src/routes/routes.go
@@ -7,16 +7,19 @@ import (
 )
 
 func BookingRoutes(router *gin.Engine, bookingHandler *handler.BookingHandler) {
-	router.GET("/bookings", middleware.Authenticate, bookingHandler.FindAll)
 	router.GET("/bookings/summaries", bookingHandler.FindAllSummaries)
 	router.POST("/bookings", bookingHandler.Create)
+	router.POST("/bookings/search", bookingHandler.Search)
+
+	// 以下のエンドポイントは認証が必要
+	router.GET("/bookings", middleware.Authenticate, bookingHandler.FindAll)
 	router.POST("/bookings/:id/approve", middleware.Authenticate, bookingHandler.Approve)
 	router.POST("/bookings/:id/cancel", middleware.Authenticate, bookingHandler.Cancel)
-	router.POST("/bookings/search", bookingHandler.Search)
 }
 
 func CarRoutes(router *gin.Engine, carHandler *handler.CarHandler) {
 	router.GET("/cars", carHandler.FindAll)
+	// 以下のエンドポイントは認証が必要
 	router.POST("/cars", middleware.Authenticate, carHandler.Create)
 	router.PATCH("/cars/:id", middleware.Authenticate, carHandler.Update)
 }
